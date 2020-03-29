1893281ae03961dcf401aef1bdeb5e3477f0f9f2
Andrelino Silva <andrelinocom@gmail.com>
2020-03-29 19:23:24 -0300

    Crianção projeto exemplo CDN Links React

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Exemplo de como usar o CDN Link do React
 
+## Links CDN usados 
+  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  
+## Exemplo de código JAVASCRIPT 
+```js
+const element = <h1>Hello World</h1>
+
+ReactDOM.render(element, document.getElementById('root'));
+```
+
+## Exemplo de código HTML 
+```html
+<div id="root"></div>
+
+<!-- Chamada do arquivo -->
+<script src="./app.js" type='text/babel'></script>
+```
+
+## Exemplo babeljs 
+```js
+  <div id="output"></div>
+  <!-- Load Babel -->
+  <!-- v6 <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script> -->
+  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+  <!-- Your custom script here -->
+  <script type="text/babel">
+  const getMessage = () => "Hello World";
+  document.getElementById('output').innerHTML = getMessage();
+  </script>
+```
+
+## Links 
+- CDN Links [CDN Links](https://pt-br.reactjs.org/docs/cdn-links.html)
+- Babel [CDN Links](https://babeljs.io/setup#installation)
---
+++ app.js
@@ -0,0 +1,68 @@
+const element = 
+    
+    <div className="container">
+        <div className="box-content">
+            <div className="search-box">
+                <h1>O que deseja encontrar?</h1>
+                <input type="search" name="search" placeHolder="Pesquisar..." id="search"/>
+            </div>
+            <div className="result-box">
+                <ul className="result-list">
+
+                    <li>
+                        <div className="result-items">
+                            <div className="application cursor">Microsft Windows</div>
+                            <div className="function cursor">Copiar seleção</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="result-items">
+                            <div className="application cursor">Microsft Windows</div>
+                            <div className="function cursor">Copiar seleção</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="result-items">
+                            <div className="application cursor">Microsft Windows</div>
+                            <div className="function cursor">Copiar seleção</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="result-items">
+                            <div className="application cursor">Microsft Windows</div>
+                            <div className="function cursor">Copiar seleção</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="result-items">
+                            <div className="application cursor">Microsft Windows</div>
+                            <div className="function cursor">Copiar seleção</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="result-items">
+                            <div className="application cursor">Microsft Windows</div>
+                            <div className="function cursor">Copiar seleção</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+
+
+
+
+    </div>
+    
+
+;
+
+
+
+ReactDOM.render(element, document.getElementById('root'));
